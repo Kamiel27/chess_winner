034520135a1633d27b945a254ef749a31fe34a6b
Michael Tuambilangana <tuambilang@gmail.com>
2024-03-11 20:43:15 +0100

    update frontend display

--- frontend/frontend_file_v1.py
+++ frontend/frontend_file_v1.py
@@ -86,5 +86,4 @@ if st.button('Predict'):
 
 	# prediction
 	response = requests.get(f"https://chesswinner-wjazvss5bq-ew.a.run.app/predict?fen={fen}").json()
-	st.write("Probability of win for white:", str(response['white']))
-    # st.write("Probability of win for black:", str(response['black']))
+	st.write(f"Probability of win for white: {response['white']}\nProbability of win for black:{response['black']}")
