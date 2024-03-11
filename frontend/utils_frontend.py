44d94b7589ebdf482e366c172d64138dd85ffe3c
Julia Dupin <julia.dupin@gmail.com>
2024-03-11 19:50:23 +0100

    Added function from_slash_to_percent to adapt to new input format for model

--- frontend/utils_frontend.py
+++ frontend/utils_frontend.py
@@ -3,15 +3,15 @@ import pandas as pd
 def fen_to_input_columns(fen):
 	# this function reorganizes the order of the rows in the FEN string so it matches the order in the columns of the input of the model
 	# after this reorganization, this function translates the ASCII characters in the FEN to a numerical classification
-	
+
 	# For the record:
-	# FEN string begins from the eighth rank (row) and ending with the first. For each rank (row), 
+	# FEN string begins from the eighth rank (row) and ending with the first. For each rank (row),
 	# squares begin from the first file (column a) and go to the eighth (column h).
 	#
 	# While the input needs to be organized as:
-	# begin from the first rank (row) and ending with the eigth one. Within each rank (row) the order is the 
+	# begin from the first rank (row) and ending with the eigth one. Within each rank (row) the order is the
 	# same as in a FEN (begin from the first file (column a) and go to the eighth (column h)).
-	
+
 	# check if input contains only valid characters
 	valid_characters = set('RrNnBbQqKkPp12345678/')
 	invalid_characters = [char for char in fen if char not in valid_characters]
@@ -20,7 +20,7 @@ def fen_to_input_columns(fen):
 		print("Valid characters are: R r N n B b Q q K k P p 1 2 3 4 5 6 7 8 /")
 		print("Please revise input FEN and try again.")
 		return
-	
+
 	# separate FEN elements
 	elements = fen.split('/')
 	#invert their order
@@ -28,7 +28,7 @@ def fen_to_input_columns(fen):
 	result = ''.join(inverted_elements)
 	# remove "/"
 	result_clean = result.replace('/', '')
-	
+
 	# now translate corrected string into numerical system to match the needed input
 	# create dictionary for pieces
 	translation_dict = {
@@ -45,7 +45,7 @@ def fen_to_input_columns(fen):
 		'P': '1',
 		'p': '7'
 		}
-	
+
 	# loop through characters in result_clean and reassign their value according to dict
 	translated_string = ''
 	for char in result_clean:
@@ -55,7 +55,7 @@ def fen_to_input_columns(fen):
 		# otherwise use dict
 		elif char in translation_dict:
 			translated_string += translation_dict[char]
-	
+
 	# check if translated_string has exactly 64 characters
 	if len(translated_string) != 64:
 		print("Error: FEN string is incomplete. Please revise and try again.")
@@ -63,7 +63,7 @@ def fen_to_input_columns(fen):
 
 	# now create df with 64 columns
 	df = pd.DataFrame(columns=[f'col{i+1}' for i in range(64)])
-	
+
 	# populate df with characters from string, make sure character is an integer
 	for i, char in enumerate(translated_string):
 		df.loc[0, i] = int(char)
@@ -76,3 +76,10 @@ def from_number_to_player(x):
 		return "white"
 	elif x == 1:
 		return "black"
+
+def from_slash_to_percent(fen):
+	# takes a regular FEN with slashes separating rows
+	# and turns slash into % symbol
+
+	fen_adjusted = fen.replace('/', '%')
+	return fen_adjusted
