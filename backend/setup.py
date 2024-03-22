947c4bc690fa5d3622231b1be3309b14d64f4aa4
Farid Machrou <fmachrou@gmail.com>
2024-03-22 17:05:27 +0100

    added CP to models

--- backend/setup.py
+++ backend/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='chess_board_64',
-      version="0.0.1",
+      version="0.1.0",
       description="Chess board in 64 dimensions",
       packages=['chess_winner'],
       )
